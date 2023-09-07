e97a5e0d3e979f990f6466262875cd6154864899
dayo <dahurdayo@example.com>
2023-09-07 17:24:29 +0100

    updated the course repesonsiveness

--- src/components/CourseDetail/CourseDetails.jsx
+++ src/components/CourseDetail/CourseDetails.jsx
@@ -4,38 +4,39 @@ import vectorBackground from "../../assets/icons/vectorbackground.png";
 import illustration from "../../assets/images/illustration.png";
 const CourseDetails = () => {
   return (
-    <div className="bg-custom-gradient  h-[800px]">
-      <div className="container flex mx-auto py-6 px-6">
+    <div className="bg-custom-gradient  h-[800px] ">
+      <div className="container lg:flex mx-auto py-6 px-6">
         <div className="relative  lg:w-1/2 w-full  space-y-8 mt-9   ">
           <button className="border border-blue-300 py-2 px-6 text-white">
             Live Programme
           </button>
 
-          <h1 className="font-bold text-white text-6xl">
+          <h1 className="font-bold text-white lg:text-6xl md:text-3xl">
             Become A Financial <br></br> Data Analyst{" "}
           </h1>
-          <p className="text-white">
+          <p className="text-white lg:text-lg md:text-xs">
             Determine The Current Production And Predict <br></br>Future Trends,
             Identify The company Financial <br></br>
             Situation And Recommend Corrective Action
           </p>
         </div>
 
-        <div className="lg:w-1/2 md:w-full flex flex-col items-center justify-center">
+        <div className="lg:w-1/2 w-full flex flex-col items-center justify-center">
           <div className="mb-4">
             <img src={illustration} alt="illustration" />
           </div>
           <div className="flex space-x-11">
-            <button className=" border-none bg-custom-button outline-none px-16 py-3 rounded-md text-white">
+            <button className="border-none bg-custom-button outline-none px-12 md:px-16 py-2 rounded-md text-white">
               Enroll
             </button>
-            <button className="border px-12 py-3 text-white ">
+
+            <button className="border px-8 md:px-12 py-2 md:py-3 text-white">
               Add To Cart
             </button>
           </div>
         </div>
       </div>
-      <div className="absolute mt-[-350px] w-[650px] py-8  space-y-4 bg-blue-600">
+      <div className="absolute mt-[-350px] w-[650px] py-8 md:block hidden space-y-4 bg-blue-600">
         <div className="flex items-center justify-between w-full px-12 ">
           <div className="flex items-center  gap-1">
             <img src={vector} alt="vector" />
--- src/components/CourseDetail/Learn.jsx
+++ src/components/CourseDetail/Learn.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import icons from "../../assets/icons/icons.png";
+import learnImages from "../../assets/images/syllabus.png";
 
 const Learn = () => {
   return (
     <div className="w-full mx-auto custom-gradient">
-      <div className="max-w-5xl mx-auto  ">
-        <h1 className="py-8 text-3xl ">What Will You Learn</h1>
-        <div className="lg:flex md:flex-row ">
-          <div className="lg:w-1/2 w-full py-6">
+      <div className="max-w-5xl mx-auto py-10  ">
+        <h1 className="py-8 text-4xl font-bold ">What Will You Learn</h1>
+        <div className="lg:flex md:flex-row lg:gap-20 md:gap-2 mt-6">
+          <div className="lg:w-2/3 w-full ">
             <div className="mb-4 flex gap-8 px-4 ">
               <img src={icons} alt="icons" className="h-[150px]" />
               <div className="bg-white px-4 space-y-3 py-4">
@@ -61,8 +62,10 @@ const Learn = () => {
               </div>
             </div>
           </div>
-          <div className="lg:w-1/2 w-full">
-            <div className="mb">hello</div>
+          <div className="lg:w-1/3  w-full">
+            <div className="flex">
+              <img src={learnImages} alt="learn" className="h-[600px]" />
+            </div>
           </div>
         </div>
       </div>
